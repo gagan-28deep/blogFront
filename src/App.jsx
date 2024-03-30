7133d03bd90e5752c2d4981c08b22caded1449bf
gagan-28deep <gagan_28_deep@outlook.com>
2024-03-30 13:08:47 +0530

    header and footer done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Outlet } from "react-router-dom";
-import { Footer, Heder } from "./components";
+import { Footer, Header } from "./components";
 
 function App() {
   return (
     <>
-      <Heder />
+      <Header />
       <main>
         <Outlet />
       </main>

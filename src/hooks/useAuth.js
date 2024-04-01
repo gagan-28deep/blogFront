46191415146fa4d62def1048136a0c1ca0f92c30
gagan-28deep <gagan_28_deep@outlook.com>
2024-04-01 11:28:29 +0530

    update on toasts

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -8,8 +8,11 @@ import {
 
 import { signup } from "../service/auth/signup";
 import { login } from "../service/auth/login";
+import { showToast } from "../utilities/toastCont";
+import { useNavigate } from "react-router-dom";
 
 const useAuth = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   // SignUp
   const handleSignUp = async (data) => {
@@ -18,8 +21,11 @@ const useAuth = () => {
       const response = await signup(data);
       dispatch(getUserData(response?.data?.data));
       dispatch(getUserDataSuccess(response?.data?.message));
+      showToast(response?.data?.message, "success");
+      navigate("/");
     } catch (error) {
       dispatch(getUserError(error?.response?.data?.message));
+      showToast(error?.response?.data?.message, "error");
     }
   };
   const handleLogin = async (data) => {
@@ -28,8 +34,11 @@ const useAuth = () => {
       const response = await login(data);
       dispatch(getUserData(response?.data?.data));
       dispatch(getUserDataSuccess(response?.data?.message));
+      showToast(response?.data?.message, "success");
+      navigate("/");
     } catch (error) {
       dispatch(getUserError(error?.response?.data?.message));
+      showToast(error?.response?.data?.message, "error");
     }
   };
   return { handleSignUp, handleLogin };
